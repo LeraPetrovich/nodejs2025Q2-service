49aafdb02a52a58cbe821269c2aee4e081bf253e
LeraPetrovich <valeria.petrovich@northitgroup.eu>
2025-05-28 22:21:09 +0300

    create types

---
+++ src/db/types.ts
@@ -0,0 +1,37 @@
+interface User {
+  id: string;
+  login: string;
+  password: string;
+  version: number;
+  createdAt: number;
+  updatedAt: number;
+}
+
+interface Artist {
+  id: string;
+  name: string;
+  grammy: boolean;
+}
+
+interface Track {
+  id: string;
+  name: string;
+  artistId: string | null;
+  albumId: string | null;
+  duration: number;
+}
+
+interface Album {
+  id: string;
+  name: string;
+  year: number;
+  artistId: string | null;
+}
+
+interface Favorites {
+  artists: string[];
+  albums: string[];
+  tracks: string[];
+}
+
+export type { User, Artist, Track, Album, Favorites };
