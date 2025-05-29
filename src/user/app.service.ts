a6e0f11ae95bfa146b4a7f12a9249d2aa313ab83
LeraPetrovich <valeria.petrovich@northitgroup.eu>
2025-05-29 11:29:20 +0300

    create user module

--- src/user/app.service.ts
+++ src/user/app.service.ts
@@ -1,8 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ForbiddenException } from '@nestjs/common';
+import type { User } from 'src/db/types';
+import { CreateUserDto } from 'src/validate/CreateUserDto';
+import { UpdatePasswordDto } from 'src/validate/UpdatePasswordDto';
+import { users } from 'src/db/db';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class AppService {
-  getUsers(): string {
-    return 'Get all users';
+  getUsers(): Array<User> {
+    return users;
+  }
+  getUser(id: string): User {
+    return users.find((item) => item.id === id);
+  }
+  createUser(credential: CreateUserDto) {
+    const id = uuidv4();
+    const date = new Date();
+    const newUser = {
+      id,
+      login: credential.login,
+      password: credential.password,
+      version: 0,
+      createdAt: date.getTime(),
+      updatedAt: date.getTime(),
+    };
+    users.push(newUser);
+    return newUser;
+  }
+
+  updateUserPassword(id: string, updatePasswordDto: UpdatePasswordDto): User {
+    const user = users.find((item) => item.id === id);
+
+    if (!user) {
+      return;
+    }
+    const { oldPassword, newPassword } = updatePasswordDto;
+    if (oldPassword !== user.password) {
+      throw new ForbiddenException('Invalid old password');
+    }
+    user.password = newPassword;
+    user.version += 1;
+    user.updatedAt = Date.now();
+    return user;
+  }
+
+  deleteUser(id: string): User {
+    const index = users.findIndex((item) => item.id === id);
+    if (index === -1) {
+      return;
+    }
+    const [deletedUser] = users.splice(index, 1);
+    return deletedUser;
   }
 }
